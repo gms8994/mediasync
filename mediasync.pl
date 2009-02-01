7caeb907860032d8ca23b80bc42b42d282eaa714
Glen Solsberry <gms8994@dp.cx>
2009-02-01 14:13:27 -0500

    Adjusted subroutine for finding path to files Adjusted table structure to include a library table

--- mediasync.pl
+++ mediasync.pl
@@ -14,16 +14,32 @@ my $songbird_library;
 
 my $sync_dbh = DBI->connect("dbi:SQLite:dbname=${home_dir}${sync_db_file}", "", "", { RaiseError => 0 }, ) or die $DBI::errstr;
 
-&query($sync_dbh, "CREATE TABLE IF NOT EXISTS hosts (id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(50) NOT NULL);", "CREATE UNIQUE INDEX IF NOT EXISTS name ON hosts (name)", "INSERT OR IGNORE INTO hosts (name) VALUES (".$sync_dbh->quote($hostname).")", "CREATE TABLE IF NOT EXISTS dirs (id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(255) NOT NULL)", "CREATE UNIQUE INDEX IF NOT EXISTS name ON dirs (name)", "CREATE TABLE IF NOT EXISTS hosts_to_dirs (host_id INTEGER, dir_id INTEGER);", "CREATE UNIQUE INDEX IF NOT EXISTS host_id_dir_id ON hosts_to_dirs (host_id, dir_id);");
+&query($sync_dbh
+                # hosts table
+                , "CREATE TABLE IF NOT EXISTS hosts (id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(50) NOT NULL);"
+                , "CREATE UNIQUE INDEX IF NOT EXISTS name ON hosts (name)"
+                , "INSERT OR IGNORE INTO hosts (name) VALUES (".$sync_dbh->quote($hostname).")"
+                # libraries table
+                , "CREATE TABLE IF NOT EXISTS libraries (id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(255) NOT NULL)"
+                , "CREATE UNIQUE INDEX IF NOT EXISTS name ON libraries (name)"
+                # hosts to libraries table
+                , "CREATE TABLE IF NOT EXISTS hosts_to_libraries (host_id INTEGER, library_id INTEGER)"
+                , "CREATE UNIQUE INDEX IF NOT EXISTS host_id_library_id ON hosts_to_libraries (host_id, library_id)"
+                # dirs table
+                , "CREATE TABLE IF NOT EXISTS dirs (id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(255) NOT NULL)"
+                , "CREATE UNIQUE INDEX IF NOT EXISTS name ON dirs (name)"
+                # libraries to dirs table
+                , "CREATE TABLE IF NOT EXISTS libraries_to_dirs (library_id INTEGER, dir_id INTEGER)"
+                , "CREATE UNIQUE INDEX IF NOT EXISTS library_id_dir_id ON libraries_to_dirs (library_id, dir_id)");
 
 my $sth = $sync_dbh->prepare("SELECT d.name FROM dirs d INNER JOIN hosts_to_dirs htd ON htd.dir_id = d.id INNER JOIN hosts h ON htd.host_id = h.id WHERE h.name = ".$sync_dbh->quote($hostname));
 $sth->execute() || die $sync_dbh->errstr;
 if (!$sth->rows()) {
     find(\&wanted, $home_dir);
     if ($songbird_library) {
-        &query($sync_dbh, "INSERT INTO dirs (name) VALUES  (".$sync_dbh->quote($songbird_library).")");
-        my $dir_id = $sync_dbh->last_insert_id(undef, undef, undef, undef);
-        &query($sync_dbh, "INSERT INTO hosts_to_dirs (dir_id, host_id) VALUES ($dir_id, (SELECT id FROM hosts WHERE name = ".$sync_dbh->quote($hostname)."))");
+        &query($sync_dbh, "INSERT INTO libraries (name) VALUES  (".$sync_dbh->quote($songbird_library).")");
+        my $library_id = $sync_dbh->last_insert_id(undef, undef, undef, undef);
+        &query($sync_dbh, "INSERT INTO hosts_to_libraries  (host_id, library_id) VALUES ((SELECT id FROM hosts WHERE name = ".$sync_dbh->quote($hostname)."), $library_id)");
     } else {
     }
 } else {
@@ -37,16 +53,11 @@ my $songbird_dbh = DBI->connect("dbi:SQLite:dbname=${songbird_library}", "", "",
 # this is the list of paths
 $sth = $songbird_dbh->prepare("SELECT media_item_id, obj FROM resource_properties WHERE property_id = 20 AND obj LIKE 'file%';");
 $sth->execute() || die $songbird_dbh->errstr;
-my $common_string = "";
+my %library_data;
 while (my ($media_item_id, $obj) = $sth->fetchrow_array()) {
-    if ($common_string ne "") {
-        my @common = &lc_substr($obj, $common_string);
-        $common_string = join("", @common);
-    } else {
-        $common_string = $obj;
-    }
+    $library_data{$media_item_id} = $obj;
 }
-die $common_string;
+my $library_path = &LCP(values(%library_data));
 
 sub query() {
     my ($dbh) = $_[0];
@@ -66,43 +77,21 @@ sub wanted {
     return 0;
 }
 
-# http://en.wikipedia.org/wiki/Longest_common_substring_problem
-sub lc_substr {
-    my ($str1, $str2) = @_; 
-    my $l_length = 0; # length of longest common substring
-    my $len1 = length $str1; 
-    my $len2 = length $str2; 
-    my @char1 = (undef, split(//, $str1)); # $str1 as array of chars, indexed from 1
-    my @char2 = (undef, split(//, $str2)); # $str2 as array of chars, indexed from 1
-    my @lc_suffix; # "longest common suffix" table
-    my @substrings; # list of common substrings of length $l_length
-
-    for my $n1 ( 1 .. $len1 ) { 
-        for my $n2 ( 1 .. $len2 ) { 
-            if ($char1[$n1] eq $char2[$n2]) {
-                # We have found a matching character. Is this the first matching character, or a
-                # continuation of previous matching characters? If the former, then the length of
-                # the previous matching portion is undefined; set to zero.
-                $lc_suffix[$n1-1][$n2-1] ||= 0;
-                # In either case, declare the match to be one character longer than the match of
-                # characters preceding this character.
-                $lc_suffix[$n1][$n2] = $lc_suffix[$n1-1][$n2-1] + 1;
-                # If the resulting substring is longer than our previously recorded max length ...
-                if ($lc_suffix[$n1][$n2] > $l_length) {
-                    # ... we record its length as our new max length ...
-                    $l_length = $lc_suffix[$n1][$n2];
-                    # ... and clear our result list of shorter substrings.
-                    @substrings = ();
-                }
-                # If this substring is equal to our longest ...
-                if ($lc_suffix[$n1][$n2] == $l_length) {
-                    # ... add it to our list of solutions.
-                    push @substrings, substr($str1, ($n1-$l_length), $l_length);
-                }
-            }
+sub LCP {
+    return '' unless @_;
+    return $_[0] if @_ == 1;
+    my $i          = 0;
+    my $first      = shift;
+    my $min_length = length($first);
+    foreach (@_) {
+        $min_length = length($_) if length($_) < $min_length;
+    }
+INDEX: foreach my $ch ( split //, $first ) {
+        last INDEX unless $i < $min_length;
+        foreach my $string (@_) {
+                last INDEX if substr($string, $i, 1) ne $ch;
         }
-    }   
-
-    return @substrings;
+    }
+    continue { $i++ }
+    return substr $first, 0, $i;
 }
-
